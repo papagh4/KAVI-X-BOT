26788151ad668b4a4ca7aad44afed13e81d6b873
papagh4 <ym3488872@gmail.com>
2025-07-18 22:20:02 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,8 +13,7 @@ global.author = process.env.AUTHOR || "Cyber Kavi"
 global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
 global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
-global.mongoDB = process.env.MONGODB_URI || "ᴘᴜᴛ ʏᴏᴜʀ ᴍᴏɴɢᴏᴅʙ ᴜʀɪ ʜᴇʀᴇ."
-
+global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://ym3488872:ym3488872@cluster0.3txowe9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0."
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
     fs.unwatchFile(file)
